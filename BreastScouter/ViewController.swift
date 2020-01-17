08e973b865b979980a8565b5c1cc52459e415756
yuuta <yuuta_cookie@icloud.com>
2020-01-18 03:20:40 +0900

    Initial Commit

---
+++ BreastScouter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BreastScouter
+//
+//  Created by 渡辺雄太 on 2020/01/18.
+//  Copyright © 2020 Yuta Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
